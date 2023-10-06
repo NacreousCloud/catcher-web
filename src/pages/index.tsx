4f9fc3331206198a3e407c008d94471887d1c2e8
김강현 <johnkim@estsoft.com>
2023-10-06 18:37:01 +0900

    Fix: src 폴더 구조 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div className="border-l">Hello!</div>;
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",

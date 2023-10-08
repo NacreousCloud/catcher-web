e1e5d650f3b65b3217c7ed38895393b58bc40876
김강현 <johnkim@estsoft.com>
2023-10-08 18:21:32 +0900

    Style: 헤더, 푸터 스타일 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return <footer className="w-full h-16 border-t-2 bottom-0 absolute"></footer>;
 };
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,24 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="w-full h-24 border-b-2">
-      <h1>Header</h1>
-      <Link href={"about-me"}>About Me</Link>
-      <Link href={"articles"}>Articles</Link>
-      <Link href={"projects"}>Projects</Link>
-      <Link href={"contact"}>Contact</Link>
+    <header className="w-full h-16 border-b-2 flex items-center justify-around">
+      <div>
+        <LinkButton href={"/"}>TEMP HEADER</LinkButton>
+      </div>
+      <div>
+        <LinkButton href={"about-me"}>About Me</LinkButton>
+        <LinkButton href={"articles"}>Articles</LinkButton>
+        <LinkButton href={"projects"}>Projects</LinkButton>
+        <LinkButton href={"contact"}>Contact</LinkButton>
+      </div>
     </header>
   );
 };
+
+const LinkButton = ({ href, children }: { href: string; children: string }) => (
+  <span className="mx-4">
+    <Link href={href}>{children}</Link>
+  </span>
+);
 
 export default Header;
